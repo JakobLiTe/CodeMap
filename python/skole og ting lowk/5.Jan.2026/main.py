7e2d4299016536b3232e92fe54a237ec7fb37b26
Jakob Lien Teigland <jakoblienteigland@Mac.hobbygnu>
2026-01-10 00:20:34 +0100

    Update 10. Jan 2026

---
+++ python/skole og ting lowk/5.Jan.2026/main.py
@@ -0,0 +1,9 @@
+def helloworld(message):
+    print(message)
+
+helloworld("print")
+
+tilbakemelding = input("Likte du denne koden? ")
+print(f"Vi sender meldingen du skrev — ´{tilbakemelding}´ til serverene våre. Takk for din tilbakemelding")
+
+print(f"Blah blah blah du er {tilbakemelding} år")
