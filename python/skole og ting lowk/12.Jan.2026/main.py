3cdb27a84be5693526e4c6dce5807de0d90a2d9f
LittleMrLazy <jakob@teig.land>
2026-01-13 02:13:48 +0100

    Added a new project to README.md with link to it via github sites

--- python/skole og ting lowk/12.Jan.2026/main.py
+++ python/skole og ting lowk/12.Jan.2026/main.py
@@ -1,13 +1,26 @@
+import time
+
 alder = input("Hvor gammel er du? ")
 
 try:
     alder = int(alder)
-    if alder >= 12:
+    if alder >= 12 and alder != 14 and alder != 15 and alder != 29:
         print("Gamle faen, hvordan er det å ha beina sine bli om til olje? Dinosaur ahh")
     elif alder < 0:
         print("Du er fortsatt i ballene til faren din")
+    elif alder == 15:
+        print("kalkulerer...")
+        time.sleep(5)
+        print("Du heter Sverre")
+    elif alder == 14:
+        print("kalkulerer...")
+        time.sleep(5)
+        print("Du heter Anne")
+    elif alder == 29:
+        print("kalkulerer...")
+        time.sleep(5)
+        print("Du heter Stian")
     else:
-        print("Kult")
+        print("Js a bebeh #so_tuff")
 except ValueError:
     print("Du må skrive inn et tall")
-
